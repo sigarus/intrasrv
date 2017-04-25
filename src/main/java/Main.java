d523ca4b0c8862bb8c3a2a11f7051dfc82ead6eb
sergey <sigarus@yandex.ru>
2017-04-25 11:37:01 +0300

    added jetty server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,28 @@
 package main.java;
 
+
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+
 /**
  * Created by SERGEY on 25.04.2017.
  */
 public class Main {
+    public static void main(String[] args) throws Exception{
+        Server server = new Server(8080);
+
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setResourceBase("htmls");
+
+        HandlerList hnd = new HandlerList();
+        hnd.setHandlers(new Handler[] {resourceHandler});
+
+        server.start();
+        System.out.println("Server started");
+
+        server.join();
+
+    }
 }
